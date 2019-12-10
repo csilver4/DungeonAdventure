406418bf2aa3877edc3d5a7e309fe4dd906c5959
Terrance <terrancejcunningham@gmail.com>
2019-12-10 09:54:29 -0800

    Added movement to the dungeonAdventure class

--- Dungeon/src/dungeon/dungeon/DungeonAdventure.java
+++ Dungeon/src/dungeon/dungeon/DungeonAdventure.java
@@ -10,91 +10,121 @@ import java.util.Scanner;
 
 public class DungeonAdventure {
 	
+    private Hero      mainHero;
+    private Dungeon   dungeon;
+    public Scanner   kb;
+	
 	public static void main(String[] args) throws Exception{
-		
+		DungeonAdventure instance = new DungeonAdventure();
+        instance.kb = new Scanner(System.in);
 		System.out.println("Dungeon Adventure!");
 		System.out.println("Explore the dungeon, find the pillars of OO, and fight off monsters");
 		System.out.println("press enter to continue, press L to load a saved game\n");
 		Hero mainHero = null;
 		Dungeon dungeon = null;
-		Scanner kb = new Scanner(System.in);
-		if(kb.nextLine().equalsIgnoreCase("l")) {
+		String userInputOnLoad = instance.kb.nextLine();
+		if(userInputOnLoad.equalsIgnoreCase("l")) {
 			try {
-				loadGame(new File("save.ser"), dungeon, mainHero);
+				instance.loadGame(new File("save.ser"), dungeon, mainHero);
 			}catch (Exception ex) {
 				System.out.println("Error opening file - " + ex.getMessage()); 
-				mainHero = Hero.factory(gameIntroduction());
+				mainHero = Hero.factory(instance.gameIntroduction());
 				dungeon = new Dungeon(5,5);
 			}
 		}else{
-			mainHero = Hero.factory(gameIntroduction());
+			mainHero = Hero.factory(instance.gameIntroduction());
 			dungeon = new Dungeon(5,5);
 		}
-		kb.close();
+		
+		
+		//kb.close();
+		
+		
 		String res;
 		do {
 			dungeon.displayRoom("current");
-			runGame(mainHero, dungeon);
+			instance.runGame(mainHero, dungeon);
 			System.out.println("do you want to play again?\n Y for yes\n anything else for no");
-			res = kb.nextLine();
+			res = instance.kb.nextLine();
 			if(res.equalsIgnoreCase("y")) {
-				mainHero = Hero.factory(gameIntroduction());
+				mainHero = Hero.factory(instance.gameIntroduction());
 				dungeon = new Dungeon(5,5);
 			}
 		}while(res.equalsIgnoreCase("y"));
 	}
+	
+	public Hero getHero()
+	{
+		return this.mainHero;
+	}
+	
+	public void setHero(Hero mainHero)
+	{
+		this.mainHero = mainHero;
+	}
+	
 
-	private static void runGame(Hero mainHero, Dungeon dungeon) throws Exception {
-		
+
+	private void runGame(Hero mainHero, Dungeon dungeon) throws Exception 
+	{
+
 		do {
-			keyPressed(new KeyEvent(null, 0, 0, 0, 0), dungeon, mainHero);
-		}while(mainHero.isAlive());
+			    System.out.print("Direction: ");
+			    String inputtedDirection = kb.nextLine().trim().toLowerCase();
+			    switch(inputtedDirection) {
+			    case"l": 
+		            moveLeft(dungeon, mainHero);
+		            break;
+			    case "r":
+		        	moveRight(dungeon, mainHero);
+		        	break;
+			    case "u": 
+		        	moveUp(dungeon, mainHero);
+		        	break;
+			    case "d": 
+		        	moveDown(dungeon, mainHero);
+		        	break;
+			    case "s":
+		        	saveGame(new File("save.ser"), dungeon, mainHero);
+		        	break;
+		        default:
+		        	System.out.println("Invalid Direction");
+			    }
+			    System.out.println("Direction Moved");
+			} while(mainHero.isAlive());			
 	}
 
-	private static String gameIntroduction() {
+	private String gameIntroduction() {
 		
 		Scanner kb = new Scanner(System.in);
-		System.out.println("LEGEND:");
-		System.out.println("Entrance: B");
-		System.out.println("Exit: Z");
-		System.out.println("Health Potion: H");
-		System.out.println("Vision Potion: V");
-		System.out.println("Pit: O");
-		System.out.println("Abstraction Pillar: A");
-		System.out.println("Encapsulation Pillar: E");
-		System.out.println("Inheritance Pillar: I");
-		System.out.println("Polymorphism Pillar P");
-		System.out.println("Door: D");
-		System.out.println("Regular Floor: #");
-		System.out.println("Character: C");
-		System.out.println("Press enter to continue...");
+		System.out.print("LEGEND:\n");
+		System.out.print("Entrance: B\n");
+		System.out.print("Exit: Z\n");
+		System.out.print("Health Potion: H\n");
+		System.out.print("Vision Potion: V\n");
+		System.out.print("Pit: O\n");
+		System.out.print("Abstraction Pillar: A\n");
+		System.out.print("Encapsulation Pillar: E\n");
+		System.out.print("Inheritance Pillar: I\n");
+		System.out.print("Polymorphism Pillar P\n");
+		System.out.print("Door: D\n");
+		System.out.print("Regular Floor: #\n");
+		System.out.print("Character: C\n");
+		System.out.print("Press enter to continue...\n");
 		kb.nextLine();
-		System.out.println("please select a new Character by entering the number");
-		System.out.println(" 1) King Arther");
-		System.out.println(" 2) Brave Sir Robin");
-		System.out.println(" 3) Zoot");
-		System.out.println(" 4) Sir Lancelot");
-		System.out.println(" 5) a Swollow");
+		System.out.print("please select a new Character by entering the number\n");
+		System.out.print(" 1) King Arther\n");
+		System.out.print(" 2) Brave Sir Robin\n");
+		System.out.print(" 3) Zoot\n");
+		System.out.print(" 4) Sir Lancelot\n");
+		System.out.print(" 5) a Swollow\n");
 		String input = kb.nextLine();
-		kb.close();
 		return input;
 	}
 	
-	public static void keyPressed(KeyEvent e, Dungeon dungeon, Hero mainHero) throws Exception {
-        Integer key = e.getKeyCode();
-        if(key == KeyEvent.VK_LEFT) 
-            moveLeft(dungeon, mainHero);
-        if(key == KeyEvent.VK_RIGHT) 
-        	moveRight(dungeon, mainHero);
-        if(key == KeyEvent.VK_UP) 
-        	moveUp(dungeon, mainHero);
-        if(key == KeyEvent.VK_DOWN) 
-        	moveDown(dungeon, mainHero);
-        if(key == KeyEvent.VK_S) 
-        	saveGame(new File("save.ser"), dungeon, mainHero);
-    }
+
 	
-	private static void saveGame(File f, Dungeon dungeon, Hero mainHero) {
+	private void saveGame(File f, Dungeon dungeon, Hero mainHero) {
 		try {
 			FileOutputStream fos = new FileOutputStream(f);
 			ObjectOutputStream oos = new ObjectOutputStream(fos);
@@ -107,7 +137,7 @@ public class DungeonAdventure {
 		}
 	}
 	
-	private static void loadGame(File f, Dungeon dungeon, Hero mainHero) throws Exception {
+	private void loadGame(File f, Dungeon dungeon, Hero mainHero) throws Exception {
 		
 		FileInputStream fis = new FileInputStream(f);
 		ObjectInputStream ois = new ObjectInputStream(fis);
@@ -117,7 +147,7 @@ public class DungeonAdventure {
 		fis.close();
 	}
 	
-	private static void moveLeft(Dungeon dungeon, Hero mainHero) throws Exception {
+	private void moveLeft(Dungeon dungeon, Hero mainHero) throws Exception {
 		
 		if(dungeon.getHeroRoomXLoc() == 0 && dungeon.getHeroRoomYLoc() == 2 && dungeon.getHeroDungeonXLoc() > 0) {
         	dungeon.setHeroRoomXLoc(4);
@@ -128,14 +158,14 @@ public class DungeonAdventure {
         	}
         	dungeon.displayRoom("current");
         }
-    	if(dungeon.getHeroRoomXLoc() > 0) {
+		else if(dungeon.getHeroRoomXLoc() > 0) {
         	dungeon.setHeroRoomXLoc(dungeon.getHeroRoomXLoc()-1);
         	dungeon.getDungeonRoom(dungeon.getHeroDungeonXLoc(), dungeon.getHeroDungeonYLoc()).getFloorTiles()[dungeon.getHeroRoomXLoc()][dungeon.getHeroRoomYLoc()].checkContains(mainHero, dungeon);
         	dungeon.displayRoom("current");
         }
 	}
 
-	private static void moveRight(Dungeon dungeon, Hero mainHero) throws Exception {
+	private void moveRight(Dungeon dungeon, Hero mainHero) throws Exception {
 		
 		if(dungeon.getHeroRoomXLoc() == 4 && dungeon.getHeroRoomYLoc() == 2 && dungeon.getHeroDungeonXLoc() < 4) {
     		dungeon.setHeroRoomXLoc(0);
@@ -146,14 +176,14 @@ public class DungeonAdventure {
         	}
     		dungeon.displayRoom("current");
     	}
-    	if(dungeon.getHeroRoomXLoc() < 4) {
+		else if(dungeon.getHeroRoomXLoc() < 4) {
         	dungeon.setHeroRoomXLoc(dungeon.getHeroRoomXLoc()+1);
         	dungeon.getDungeonRoom(dungeon.getHeroDungeonXLoc(), dungeon.getHeroDungeonYLoc()).getFloorTiles()[dungeon.getHeroRoomXLoc()][dungeon.getHeroDungeonYLoc()].checkContains(mainHero, dungeon);
         	dungeon.displayRoom("current");
     	}
 	}
 
-	private static void moveUp(Dungeon dungeon, Hero mainHero) throws Exception {
+	private void moveUp(Dungeon dungeon, Hero mainHero) throws Exception {
 		
 		if(dungeon.getHeroRoomXLoc() == 2 && dungeon.getHeroRoomYLoc() == 4 && dungeon.getHeroDungeonYLoc() < 4) {
     		dungeon.setHeroRoomYLoc(0);
@@ -164,14 +194,14 @@ public class DungeonAdventure {
         	}
     		dungeon.displayRoom("current");
     	}
-    	if(dungeon.getHeroRoomYLoc() < 4) {
+		else if(dungeon.getHeroRoomYLoc() < 4) {
         	dungeon.setHeroRoomYLoc(dungeon.getHeroRoomYLoc());
         	dungeon.getDungeonRoom(dungeon.getHeroDungeonXLoc(), dungeon.getHeroDungeonYLoc()).getFloorTiles()[dungeon.getHeroRoomXLoc()][dungeon.getHeroDungeonYLoc()].checkContains(mainHero, dungeon);
         	dungeon.displayRoom("current");
     	}
 	}
 
-	private static void moveDown(Dungeon dungeon, Hero mainHero) throws Exception {
+	private void moveDown(Dungeon dungeon, Hero mainHero) throws Exception {
 		
 		if(dungeon.getHeroRoomXLoc() == 2 && dungeon.getHeroRoomYLoc() == 0 && dungeon.getHeroDungeonYLoc() > 0) {
     		dungeon.setHeroRoomYLoc(4);
@@ -182,7 +212,7 @@ public class DungeonAdventure {
         	}
     		dungeon.displayRoom("current");
     	}
-    	if(dungeon.getHeroRoomYLoc() > 0) {
+		else if(dungeon.getHeroRoomYLoc() > 0) {
         	dungeon.setHeroRoomYLoc(dungeon.getHeroRoomYLoc()-1);
         	dungeon.getDungeonRoom(dungeon.getHeroDungeonXLoc(), dungeon.getHeroDungeonYLoc()).getFloorTiles()[dungeon.getHeroRoomXLoc()][dungeon.getHeroDungeonYLoc()].checkContains(mainHero, dungeon);
         	dungeon.displayRoom("current");
--- Dungeon/src/dungeon/dungeon/Monster.java
+++ Dungeon/src/dungeon/dungeon/Monster.java
@@ -41,21 +41,23 @@ public class Monster extends DungeonCharacter{
 		super.attack(opponent);
 	}
 
+	//TODO: Uncomment Chester's comment
 	public static Monster monsterFactory() {
 	
-		int res = (int) (Math.random()*5);
-		if(res == 0)
-			return new Monster("The Black Knight", 200, 2, .6, .1, 30, 50, 30, 50);
-		if(res == 1)
-			return new Monster("Tom The Echanter", 70, 5, .8, .4, 15, 30, 20, 40);
-		if(res == 2)
-			return new Monster("The French Taunters", 100, 3, .8, .3, 30, 50, 30, 50);
-		if(res == 3)
-			return new Monster("The French Taunters", 100, 3, .8, .3, 30, 50, 30, 50);
-		if(res == 4)
-			return new Monster("The French Taunters", 100, 3, .8, .3, 30, 50, 30, 50);
-		else
-			return new Monster("The Black Knight", 200, 2, .6, .1, 30, 50, 30, 50);
+		return null;
+//		int res = (int) (Math.random()*5);
+//		if(res == 0)
+//			return new Monster("The Black Knight", 200, 2, .6, .1, 30, 50, 30, 50);
+//		if(res == 1)
+//			return new Monster("Tom The Echanter", 70, 5, .8, .4, 15, 30, 20, 40);
+//		if(res == 2)
+//			return new Monster("The French Taunters", 100, 3, .8, .3, 30, 50, 30, 50);
+//		if(res == 3)
+//			return new Monster("The French Taunters", 100, 3, .8, .3, 30, 50, 30, 50);
+//		if(res == 4)
+//			return new Monster("The French Taunters", 100, 3, .8, .3, 30, 50, 30, 50);
+//		else
+//			return new Monster("The Black Knight", 200, 2, .6, .1, 30, 50, 30, 50);
 	}
 }
 
